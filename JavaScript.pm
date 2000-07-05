a7cae2c8d3bb595b2b3875d6de95e994f637cc43
Ariel Brosh (PAUSE Custodial Account) <modules@perl.org>
2000-07-05 16:15:41 +0000

    Import of SCHOP/Data-JavaScript-1.03 from CPAN.
    gitpan-cpan-distribution: Data-JavaScript
    gitpan-cpan-version:      1.03
    gitpan-cpan-path:         SCHOP/Data-JavaScript-1.03.tar.gz
    gitpan-cpan-author:       SCHOP
    gitpan-cpan-maturity:     released

--- JavaScript.pm
+++ JavaScript.pm
@@ -10,7 +10,7 @@ require Exporter;
 @EXPORT = qw(
     jsdump hjsdump
 );
-$VERSION = '1.00';
+$VERSION = '1.03';
 
 sub jsdump {
     my $sym = shift;
@@ -68,4 +68,51 @@ sub __jsdump {
     }
 }
 
+
 1;
+__END__
+
+# Below is the stub of documentation for your module. You better edit it!
+
+=head1 NAME
+
+Data::JavaScript - Perl extension for dumping structures into JavaScript
+code
+
+=head1 SYNOPSIS
+
+  use Data::JavaScript;
+  
+  @code = jsdump($array_ref, 'my_array');
+  $code = jsdump($hash_ref, 'my_object');
+  $code = hjsdump($array_ref B<or> $hash_ref, 'my_stuff');
+
+=head1 DESCRIPTION
+
+This module is aimed mainly for CGI programming, when a perl script
+generates a page with client side JavaScript code that needs access to
+structures created on the server.
+
+It works by creating one line of JavaScript code per datum. Therefore,
+structures cannot be created anonymously and needed to be assigned to
+variables. This enables dumping big structures.
+
+The first arguement is a hashref or arrayref. Structures can be nested,
+circular referrencing is supported EXPERIMENTALLY only.
+The second argument is the name of JavaScript object to create.
+
+hsdump also dumps HTML tags to embed the scriplet inside an HTML page.
+
+When called in array context, the functions return an array of code
+lines. When called in scalar context, it returns one chunk of lines
+delimited by line fields.
+
+=head1 AUTHOR
+
+Ariel Brosh, schop@cpan.org. Inspired by WDDX.pm JavaScript support.
+
+=head1 SEE ALSO
+
+perl(1), L<WDDX>.
+
+=cut
